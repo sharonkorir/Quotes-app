f74b819a4944124136ca3cfcd471a86bc6ab4723
sharonkorir <sharon.korir@student.moringaschool.com>
2022-02-05 13:26:26 +0300

    fix show more button

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,5 @@
 export class Quote {
-  showDescription: boolean;
+  public showDescription: boolean;
   constructor(public quote: string,public author: string,public movie: any,public submitter: string){
     this.showDescription=false;
   }
