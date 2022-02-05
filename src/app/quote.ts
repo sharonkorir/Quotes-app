ec32658264c71431d68c6b0afdfd06d2342a46b9
sharonkorir <sharon.korir@student.moringaschool.com>
2022-02-05 10:29:26 +0300

    create quote component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,5 @@
 export class Quote {
   quote!: string;
   author!: string;
+  submitter!: string;
 }
---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+
+  quotes:Quote[] = [
+    {quote:'', author: '', submitter: ''},
+    {quote:'', author: '', submitter: ''},
+    {quote:'', author: '', submitter: ''},
+    {quote:'', author: '', submitter: ''},
+    {quote:'', author: '', submitter: ''},
+
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
