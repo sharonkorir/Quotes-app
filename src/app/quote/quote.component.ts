bd022b88db2132ba1accafd797f4cf4189ebaba0
sharonkorir <sharon.korir@student.moringaschool.com>
2022-02-05 12:30:15 +0300

    update quote submitter variable

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,11 +10,11 @@ export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
 
-    {quote:'"Do or do not. There is no try."', author: 'Yoda', movie: 'Star Wars', submitter: ''},
-    {quote:'"Happiness can be found even in the darkest of times, if one only remembers to turn on the light."', author: 'Dumbledore', movie: 'Harry Porter and the Prisoner of Azkaban', submitter: ''},
-    {quote:'“The flower that blooms in adversity is the most rare and beautiful of all.”', author: 'Fa Zhou', movie: 'Mulan', submitter: ''},
-    {quote:'"When life gets you down do you wanna know what you\'ve gotta do? Just keep swimming!"', author: 'Dory', movie: 'Finding Nemo', submitter: ''},
-    {quote:'"Our lives are defined by opportunities, even the ones we miss."', author: 'Benjamin Button', movie: 'The Curious Case of Benjamin Button', submitter: ''},
+    {quote:'"Do or do not. There is no try."', author: 'Yoda', movie: 'Star Wars', submitter: 'Nick'},
+    {quote:'"Happiness can be found even in the darkest of times, if one only remembers to turn on the light."', author: 'Dumbledore', movie: 'Harry Porter and the Prisoner of Azkaban', submitter: 'Sky'},
+    {quote:'“The flower that blooms in adversity is the most rare and beautiful of all.”', author: 'Fa Zhou', movie: 'Mulan', submitter: 'Patricia'},
+    {quote:'"When life gets you down do you wanna know what you\'ve gotta do? Just keep swimming!"', author: 'Dory', movie: 'Finding Nemo', submitter: 'Jessie'},
+    {quote:'"Our lives are defined by opportunities, even the ones we miss."', author: 'Benjamin Button', movie: 'The Curious Case of Benjamin Button', submitter: 'Diana'},
     
   ];
 
